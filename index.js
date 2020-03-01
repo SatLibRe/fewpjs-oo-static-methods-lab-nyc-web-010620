ab998f0ffc1c0b2ecba848a575c0f3941f4a2846
Miles Higbie <miles.higbie@gmail.com>
2020-03-01 18:58:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 class Formatter {
-  //add static methods here
+  static capitalize(name){
+    return name.charAt(0).toUpperCase() + name.slice(1)
+  }
+  
+  static sanitize(word){
+    return word.replace(/[^0-9\-]/g,'')
+  }
 }
